7c2ca5284568f0a87880c66c2501c1b1a06101e0
Bryan <bryanpaicoalbines97@gmail.com>
2026-05-26 23:20:30 +0200

    chore: scaffold spring boot 4 project on java 25 with web, validation and springdoc

---
+++ src/main/java/com/bryan/apiplayground/ApiPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.bryan.apiplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPlaygroundApplication.class, args);
+    }
+}
