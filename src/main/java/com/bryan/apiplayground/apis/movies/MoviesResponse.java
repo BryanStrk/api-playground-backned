a2e026da534e426c493dcc78783598240fc45751
BryanStrk <126487933+BryanStrk@users.noreply.github.com>
2026-05-28 13:41:06 +0200

    feat: complete movies response implementacion

--- src/main/java/com/bryan/apiplayground/apis/movies/MoviesResponse.java
+++ src/main/java/com/bryan/apiplayground/apis/movies/MoviesResponse.java
@@ -2,5 +2,10 @@ package com.bryan.apiplayground.apis.movies;
 
 import java.util.List;
 
-public record MoviesResponse(int page, List<Movie> movies, int totalPages, int totalResults) {
+public record MoviesResponse(
+        int page,
+        List<Movie> movies,
+        int totalPages,
+        int totalResults
+) {
 }
